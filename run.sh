ccbd835c0b278ab79b0f0a5063ba69fb4d3f76e8
rhel8 <developer@ansible.workstation.examplelab.com>
2019-10-08 07:12:52 -0700

    tower cleanup

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-ansible-playbook --connection=local --inventory 127.0.0.1, main.yml
+#ansible-playbook --connection=local --inventory 127.0.0.1, main.yml
+ansible-playbook -i hosts main.yml -u root
